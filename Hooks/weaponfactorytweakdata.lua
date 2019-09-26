01e3ca79f38e3af802d1ee4463801fa442c1c33b
Alcat101 <adhanika15@gmail.com>
2019-09-27 00:02:57 +0700

    Ditched mat_cfg file only method
    override cant change material_config tweak data
    so reverted back to normal override method, with the usual 8 asset file changes
    
    adds rack attachment to other vanilla pump-action shotgun
    by reusing r870 rack
    added to the uses_parts with append, and overrode the stat and a_obj

--- Hooks/weaponfactorytweakdata.lua
+++ Hooks/weaponfactorytweakdata.lua
@@ -1,19 +1,51 @@
 Hooks:PostHook( WeaponFactoryTweakData, "init", "ShellrackModInit", function(self)
 
-	--[[this is how the shell rack will change the shell texture according to equipped shotgun ammo
-	a_HE override = {
-		self.parts.wpn_fps_shot_ben_upg_rack.material_config = Idstring("units/mods/weapons/HEslug_texture/r870_HE_rack")
-		self.parts.wpn_fps_shot_ben_upg_rack.thq_material_config = Idstring("units/mods/weapons/HEslug_texture/r870_HE_rack_thq")
-		self.parts.wpn_fps_shot_ben_upg_rack.cc_material_config = Idstring("units/mods/weapons/HEslug_texture/r870_HE_rack_cc")
-		self.parts.wpn_fps_shot_ben_upg_rack.cc_thq_material_config = Idstring("units/mods/weapons/HEslug_texture/r870_HE_rack_cc_thq")
+	--giving other pump-action shotgun the shell rack attch
+	--benelli m1014
+	table.list_append(self.wpn_fps_sho_ben.uses_parts, {"wpn_fps_shot_r870_body_rack"})
+	table.list_append(self.wpn_fps_sho_ben_npc.uses_parts, {"wpn_fps_shot_r870_body_rack"})
+	self.wpn_fps_sho_ben.override = self.wpn_fps_sho_ben.override or {}
+	self.wpn_fps_sho_ben.override.wpn_fps_shot_r870_body_rack = {
+		a_obj = "a_rack_benelli",
+		stats = {
+			value = 3,
+			spread_moving = -1,
+			concealment = -2,
+			total_ammo_mod = 2
+		}
 	}
-	]]--
+	--spas12
+	table.list_append(self.wpn_fps_sho_spas12.uses_parts, {"wpn_fps_shot_r870_body_rack"})
+	table.list_append(self.wpn_fps_sho_spas12_npc.uses_parts, {"wpn_fps_shot_r870_body_rack"})
+	self.wpn_fps_sho_spas12.override = self.wpn_fps_sho_spas12.override or {}
+	self.wpn_fps_sho_spas12.override.wpn_fps_shot_r870_body_rack = {
+		a_obj = "a_rack_spas12",
+		stats = {
+			value = 3,
+			spread_moving = -1,
+			concealment = -2,
+			total_ammo_mod = 2
+		}
+	}
+	--ksg
+	table.list_append(self.wpn_fps_sho_ksg.uses_parts, {"wpn_fps_shot_r870_body_rack"})
+	table.list_append(self.wpn_fps_sho_ksg_npc.uses_parts, {"wpn_fps_shot_r870_body_rack"})
+	self.wpn_fps_sho_ksg.override = self.wpn_fps_sho_ksg.override or {}
+	self.wpn_fps_sho_ksg.override.wpn_fps_shot_r870_body_rack = {
+		a_obj = "a_rack_ksg",
+		stats = {
+			value = 3,
+			spread_moving = -1,
+			concealment = -2,
+			total_ammo_mod = 4
+		}
+	}
 	
-	--
+	
+	--for loop to apply different shell color texture
 	local rack_matcfg = {
 		["wpn_fps_shot_r870_body_rack"] = "units/payday2/weapons/wpn_fps_shot_r870_pts/wpn_fps_shot_r870_body_rack",
-		["wpn_fps_shot_b682_s_ammopouch"] = "units/pd2_dlc_bonnie/weapons/wpn_fps_shot_b682_pts/wpn_fps_shot_b682_s_ammopouch",
-		["wpn_fps_shot_ben_upg_rack"] = "units/payday2/weapons/wpn_fps_shot_r870_pts/wpn_fps_shot_r870_body_rack"
+		["wpn_fps_shot_b682_s_ammopouch"] = "units/pd2_dlc_bonnie/weapons/wpn_fps_shot_b682_pts/wpn_fps_shot_b682_s_ammopouch"
 	}
 	
 	local shell_id = {
@@ -28,27 +60,14 @@ Hooks:PostHook( WeaponFactoryTweakData, "init", "ShellrackModInit", function(sel
 			["wpn_fps_upg_a_dragons_breath"] = "units/mods/weapons/Dragonsbreath_texture/b682_Dragon_ammopouch",
 			["wpn_fps_upg_a_piercing"] = "units/mods/weapons/Flechette_texture/b682_Flechette_ammopouch",
 			["wpn_fps_upg_a_explosive"] = "units/mods/weapons/HEslug_texture/b682_HE_ammopouch"
-		},
-		["wpn_fps_shot_ben_upg_rack"] = {
-			["wpn_fps_upg_a_slug"] = "units/mods/weapons/APslug_texture/r870_AP_rack",
-			["wpn_fps_upg_a_dragons_breath"] = "units/mods/weapons/Dragonsbreath_texture/r870_Dragon_rack",
-			["wpn_fps_upg_a_piercing"] = "units/mods/weapons/Flechette_texture/r870_Flechette_rack",
-			["wpn_fps_upg_a_explosive"] = "units/mods/weapons/HEslug_texture/r870_HE_rack"
 		}
 	}
 	for rack_id, mat_id in pairs(rack_matcfg) do
-		self.parts[rack_id].material_config = Idstring( mat_id )
-		self.parts[rack_id].thq_material_config = Idstring( mat_id .. "_thq" )
-		self.parts[rack_id].cc_material_config = Idstring( mat_id .. "_cc" )
-		self.parts[rack_id].cc_thq_material_config = Idstring( mat_id .. "_cc_thq" )
 		for ammo_id, matcfg_path in pairs(shell_id[rack_id]) do
 			if self.parts[ammo_id] then
 				self.parts[ammo_id].override = self.parts[ammo_id].override or {}
-				self.parts[ammo_id].override[rack_id] = {
-					material_config = Idstring( matcfg_path ),
-					thq_material_config = Idstring( matcfg_path .. "_thq" ),
-					cc_material_config = Idstring( matcfg_path .. "_cc" ),
-					cc_thq_material_config = Idstring( matcfg_path .. "_cc_thq" )
+				self.parts[ammo_id].override[rack_id] = { 
+					unit = matcfg_path
 				}
 			end
 		end
--- Hooks/weapontweakdata.lua
+++ Hooks/weapontweakdata.lua
@@ -1,10 +1,24 @@
 Hooks:PostHook(WeaponTweakData, "init", "ShellrackModInit", function(self)
 
 	self:SetupAttachmentPoint( "benelli", {
-		name = "a_body",
-		base_a_obj = "a_rack_benelli",
-		position = Vector3( 0, 0, 0 ),
+		name = "a_rack_benelli",
+		base_a_obj = "a_body",
+		position = Vector3( 0, 21.5, 7.25 ),
 		rotation = RotationCAP(0, 0, 0)
+	})
+
+	self:SetupAttachmentPoint( "spas12", {
+		name = "a_rack_spas12",
+		base_a_obj = "a_body",
+		position = Vector3( 0, 19, 8 ),
+		rotation = RotationCAP(0, 0, 0)
+	})
+
+	self:SetupAttachmentPoint( "ksg", {
+		name = "a_rack_ksg",
+		base_a_obj = "a_body",
+		position = Vector3( 1, -10.5, 6 ),
+		rotation = RotationCAP(0, 0, 160)
 	})
 
 end)
